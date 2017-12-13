ad66a004c03dfb29758e8dc9e7bfb5762a1782e2
Tiwarii <tiwarirashmi042@gmail.com>
2017-12-13 14:26:33 +0545

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+
+var config = {
+    entry: './main.js',
+    output: {
+        filename: 'index.js',
+        path: __dirname
+            },
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
+module.exports = config;
